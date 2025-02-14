36dac7a237083f6a8365c65123dbda184f770e0a
KermX <KermitVIII@gmail.com>
2025-02-13 19:11:23 -0600

    Initial commit

---
+++ src/main/java/me/kermx/seedBags/SeedBagCommandExecutor.java
@@ -0,0 +1,4 @@
+package me.kermx.seedBags;
+
+public class SeedBagCommandExecutor {
+}
---
+++ src/main/java/me/kermx/seedBags/SeedBags.java
@@ -0,0 +1,17 @@
+package me.kermx.seedBags;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SeedBags extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
